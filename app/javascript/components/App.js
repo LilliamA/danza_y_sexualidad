9ea580f406c3b59a981d0ddf0f440ef092c1584d
LilliamA <lilliam.arce31@gmail.com>
2020-04-10 21:10:54 -0600

    Added react initial setup and smoke test

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,9 +1,16 @@
 import React from "react"
 import PropTypes from "prop-types"
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import Home from './Home'
 class App extends React.Component {
   render () {
     return (
       <React.Fragment>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/about" render={ ()=> <Home></Home>}/>
+          </Switch>
+        </BrowserRouter>
       </React.Fragment>
     );
   }
